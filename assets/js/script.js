9d6d7d47b1ac3ff29fc97d2d0f5eb0b7f1736862
Marcelo Lisboa Cherem <marcelo@Marcelos-MacBook-Pro.local>
2024-11-29 14:40:41 +0100

    create login space

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,31 @@ const loginArea = document.querySelector('.loginArea');
 const overLay = document.querySelector('.overlay');
 
 cartLinks.forEach(cartLink => {
-    cartLink.addEventListener('click', function(event) {
-      event.preventDefault();
-      loginArea.classList.toggle('open');
-      overLay.classList.toggle('blur');
-    });
+  cartLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    loginArea.classList.toggle('open');
+    overLay.classList.toggle('blur');
   });
+});
 
-overLay.addEventListener("click", function(event) {
-    loginArea.classList.remove('open');
-    this.classList.remove('blur');
+overLay.addEventListener("click", function (event) {
+  loginArea.classList.remove('open');
+  this.classList.remove('blur');
+});
+
+//login/signup
+
+const signupArea = document.querySelector('.signUpArea');
+const signupButton = document.querySelector('.signupBtn');
+const loginAreabtn = document.querySelector('.loginAreaBtn');
+const formLogin = document.querySelector('.loginPart');
+
+signupButton.addEventListener("click", function (event) {
+  signupArea.classList.add('signupUp');
+  formLogin.classList.add('desapear');
+});
+loginAreabtn.addEventListener("click", function (event) {
+  signupArea.classList.remove('signupUp');
+  formLogin.classList.remove('desapear');
+
 });
